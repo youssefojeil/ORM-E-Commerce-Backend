39254b259da60160aa0b957e8a8c09638b18b51c
unknown <youssef.ojeil@hotmail.com>
2022-11-06 15:57:28 -0500

    setup productTag model attributes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,19 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    product_tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        id: 'product_id'
+      },
+    },
   },
   {
     sequelize,
