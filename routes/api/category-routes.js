852479eaf92ba91c7ef86d1c018676149e4482b1
unknown <youssef.ojeil@hotmail.com>
2022-11-06 17:51:15 -0500

    category delete and update complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { regexp } = require('sequelize/types/lib/operators');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -56,12 +55,46 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update({ category_name: req.body.category_name}, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!categoryData) {
+      res.status(404).json({ message: 'No Category found with that id!'})
+      return;
+    }
+
+    res.status(200).json(categoryData);
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+  const categoryData = await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  
+  if (!categoryData) {
+    res.status(404).json({ message: 'No Category found with that id!'});
+    return;
+  }
+ 
+  res.status(200).json(categoryData);
+  
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
