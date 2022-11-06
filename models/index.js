d44098830ca7f74e4de4c35901d790db95080c1e
unknown <youssef.ojeil@hotmail.com>
2022-11-06 16:53:32 -0500

    fix association syntax

--- models/index.js
+++ models/index.js
@@ -5,10 +5,10 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo.Category;
+Product.belongsTo(Category);
 
 // Categories have many Products
-Category.hasMany.Product;
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {through: 'ProductTag'});
