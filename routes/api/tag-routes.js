4224a5e7abfbb5dd2d14c4d8d2350aa3de9447e5
unknown <youssef.ojeil@hotmail.com>
2022-11-06 19:43:44 -0500

    update tag name through tag put route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -54,8 +54,25 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update({ tag_name: req.body.tag_name}, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!tagData) {
+      res.status(404).json({ message: 'No Tag found with that id'});
+      return;
+    }
+
+    res.status(200).json(tagData);
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
